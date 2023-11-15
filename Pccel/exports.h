e0b6a6ed84447125a41ad32956feb3ddfbf2d556
noahw2021 <noah.wooten@icloud.com>
2023-11-15 03:23:50 -0500

    Add project files.

---
+++ Pccel/exports.h
@@ -0,0 +1,12 @@
+#pragma once
+/*
+exports.h
+Pccl
+
+PLASM2 Compiled C Emulation Layer
+(c) Noah Wooten 2023, All Rights Reserved
+*/
+
+#include <Windows.h>
+
+#define CCEL_API __declspec(dllexport)
